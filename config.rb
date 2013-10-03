8d55d43b08672a156aef95bb89ca6e37c8c8b39f
Philip Meissner <phil@twopaperdolls.com>
2013-10-03 14:01:04 -0400

    removing the line comments

--- config.rb
+++ config.rb
@@ -15,7 +15,7 @@ environment = :development
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = true
+# line_comments = true
 color_output = false
 
 # If you prefer the indented syntax, you might want to regenerate this
